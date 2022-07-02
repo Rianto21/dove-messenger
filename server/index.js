683b16fb38c8f7d6cc62a5fba0487e6c96ae9463
Oryto Mikli <orytomikli21@gmail.com>
2022-07-02 23:50:23 +0700

    fix socket io

--- server/index.js
+++ server/index.js
@@ -2,25 +2,28 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import http, { Server } from 'http'
+// import http, { Server } from 'http'
 import dotenv from 'dotenv';
+import * as socket from 'socket.io'
+
 dotenv.config();
 
 //local Modules
 import postroutes from './routes/post.js'
 import userroutes from './routes/users.js'
 import messageroutes from './routes/message.js'
-import * as socket from 'socket.io'
-
 
 const app = express();
 const router = express.Router();
-const io = socket(server);
-io.on('connection', client => {
-  client.on('event', data => { /* … */ });
-  client.on('disconnect', () => { /* … */ });
+const io = new socket.Server({});
+io.on('connection', (server) => {
+  console.log(server.id)
+  server.on("send-message", message => {
+    server.broadcast.emit("receive-message", message)
+    console.log(message)
+  })
 });
-server.listen(8080);
+io.listen(8080)
 
 app.use(cors())
 app.use(bodyParser.json({limit: "30mb", extended: true}));
