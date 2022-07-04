d15037c00c02ccb9ff1805ce3cf6180e9249f5c7
Oryto Mikli <orytomikli21@gmail.com>
2022-07-04 17:38:58 +0700

    fix socket io for message

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,2 +1,49 @@
-import * as io from 'socket.io';
+import * as socket from 'socket.io';
 
+let users = [];
+
+
+const io = new socket.Server(8080, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+})
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //when ceonnect
+  console.log("a user connected.");
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
